52484e7baafd1f7fe4da6fb7cedffd0f383c87c5
Stefan Schneider <schneist@gmail.com>
2019-07-20 11:51:41 +0200

    implicits

--- functions/src/main/scala/org/novogarchinsk/functions/google/cloud/firestore/FireStoreStore.scala
+++ functions/src/main/scala/org/novogarchinsk/functions/google/cloud/firestore/FireStoreStore.scala
@@ -1,22 +1,17 @@
 package org.novogarchinsk.functions.google.cloud.firestore
 
-import com.sun.source.doctree.SerialFieldTree
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.boolean.And
 import eu.timepit.refined.collection.{MaxSize, MinSize}
 import org.novogarchinsk.functions.google.cloud.firestore.FirebaseFirestore.{DocumentData, DocumentSnapshot}
 import org.novogarchinsk.functions.{TimeSeriesStore, _}
 import scalaz.zio.{IO, ZIO}
-import shapeless.{::, tag, _}
-import shapeless.labelled.{FieldBuilder, FieldType, KeyTag}
-import record._
-import ops.record._
-import org.novogarchinsk.functions.google.cloud.firestore.gcloud.Extractor.SimpleExtractor
-import shapeless.Witness.Aux
-import syntax.singleton._
+import shapeless._
+import shapeless.labelled._
+import shapeless.tag._
+import shapeless.syntax.singleton._
 
-import scala.language.higherKinds
-import scala.language.existentials
+import scala.language.{existentials, higherKinds, implicitConversions}
 import scala.reflect.ClassTag
 import scala.scalajs.js
 
@@ -56,54 +51,52 @@ object gcloud {
     }
   }
 
-  trait Extractor[V]{
+  trait FieldTypeExtractor[V]{
 
-    def from[K](doc:DocumentData)(implicit ev: Witness.Aux[K]) : V with labelled.KeyTag[Symbol with tag.Tagged[K], V]
+
+    def from[K <: Symbol ](doc:DocumentData)(implicit ev: Witness.Aux[K]) : FieldType[K, V]
 
   }
 
-  implicit object Extractor{
+  object FieldTypeExtractor{
 
-    class SimpleExtractor[V] extends Extractor[V]{
+    class SimpleFieldTypeExtractor[V] extends FieldTypeExtractor[V]{
 
       def singletonValueName[K <: Symbol](implicit ev: Witness.Aux[K]): String = ev.value.name
 
-      override def from[K](doc: DocumentData)(implicit ev: Witness.Aux[K]) : V with labelled.KeyTag[Symbol with tag.Tagged[K], V]  = {
-        val name :String =  ??? // singletonValueName[K]
-
+      override def from[K<:Symbol](doc: DocumentData)(implicit ev: Witness.Aux[K]):FieldType[K,V] = {
+        val name =  singletonValueName[K]
         val v :V = doc.get(name).asInstanceOf[V]
-
-        val ret = new FieldBuilder[Symbol with tag.Tagged[K]].apply(v)
-        ret
+        new FieldBuilder[K].apply(v)
       }
 
     }
 
     // "Summoner" method
-    def apply[T](implicit pack: Extractor[T]): Extractor[T] = pack
+    implicit def apply[T](implicit pack: FieldTypeExtractor[T]): FieldTypeExtractor[T] = pack
 
     // "Constructor" method
-    def instance[T]: Extractor[T] = new SimpleExtractor[T]
+    implicit def instance[T]: FieldTypeExtractor[T] = new SimpleFieldTypeExtractor[T]
 
 
   }
 
   abstract class ReprFact[R ]{
-    def getRepr(documentSnapshot: DocumentData)(implicit ev: Witness.Aux[_]) :R
+    def getRepr(documentSnapshot: DocumentData) :R
   }
 
   object ReprFact {
 
-    implicit def NilRepr: ReprFact[HNil] = new ReprFact[HNil] {
-      override def getRepr(documentSnapshot: DocumentData)(implicit ev: Aux[_]) = HNil
+    implicit def NilRepr: ReprFact[HNil] = _ => HNil
+
+
+    implicit def SingleRepr[K<: Symbol,V](
+                                           implicit extractor: FieldTypeExtractor[V],
+                                           ev: Witness.Aux[K]
+                                         ) = new ReprFact[V] {
+      override def getRepr(documentSnapshot: DocumentData) =
+        extractor.from(documentSnapshot)(ev)
     }
-
-
-    implicit def SingleRepr[K <:Symbol,V <: KeyTag[Symbol with tag.Tagged[K], V] ](implicit extractor: Extractor[V],ev: Witness.Aux[K]):ReprFact[V] =
-      new ReprFact[V] {
-        override def getRepr(documentSnapshot: DocumentData)(implicit ev: Witness.Aux[_]) =
-          extractor.from(documentSnapshot)(ev.asInstanceOf[Witness.Aux[K]])
-      }
 
     implicit def PairRepr[Head , Tail <: HList ](
                                                   implicit reprFactH: ReprFact[Head],
@@ -117,7 +110,7 @@ object gcloud {
   object GCTimeSeriesStore extends GCFireStore[TimeValue](conv =  (a:DocumentSnapshot) => {
     a.data().fold(
       aa => ZIO.apply {
-        val gen = LabelledGeneric[TimeValue]
+
 
         //val fieldN: List[String] = storedDocument.fieldNames()
 
@@ -125,35 +118,58 @@ object gcloud {
         //ToDo:: add Extractor for HNil
         //ToDo:: add Extractor for  Decoder[FieldType[K, V] :+: R]
 
+        type time = Witness.`"time"`.T
+        type value = Witness.`"value"`.T
 
-        type ds =
-          Long with labelled.KeyTag[Symbol with shapeless.tag.Tagged[Witness.`"time"`.T], Long]::
-            Double with labelled.KeyTag[Symbol with shapeless.tag.Tagged[Witness.`"value"`.T], Double]::
-            HNil
-        import ReprFact._
-        implicitly[ReprFact[HNil]]
+        type timeS = @@[Symbol,time]
+        type valueS = Symbol with shapeless.tag.Tagged[value]
+
+        type timeFT = FieldType[timeS, Long]
+        type valueFT = FieldType[valueS, Double]
 
 
-        implicit val v = new ReprFact[Long with labelled.KeyTag[Symbol with shapeless.tag.Tagged[Witness.`"time"`.T], Long]] {
-          override def getRepr(documentSnapshot: DocumentData)(implicit ev: Aux[_]) = ???
+        type ds = timeFT :: valueFT :: HNil
+
+        implicit val ftet  :FieldTypeExtractor[timeFT] = FieldTypeExtractor.instance[timeFT]
+        implicit val ftev  :FieldTypeExtractor[valueFT] = FieldTypeExtractor.instance[valueFT]
+
+
+        implicit def v[TT](implicit tv : Witness.Aux[TT]) = new Witness {
+
+          override type T = @@[Symbol,TT]
+
+          override val value =  tag.apply[scala.Symbol].apply(tv.value).asInstanceOf[@@[Symbol,TT]]
         }
+
+        implicit val repT : ReprFact[timeFT] = ReprFact.SingleRepr[timeS ,timeFT]
+
+        val a :timeFT = repT.getRepr(aa)
+        /*
+
+        //implicitly[Extractor[Long with labelled.KeyTag[Symbol with shapeless.tag.Tagged[Witness.`"time"`.T], Long]]]
+
+        implicit val llll = ReprFact.SingleRepr[timeS,kt]
+
+        //implicitly[ReprFact[Long with labelled.KeyTag[Symbol with shapeless.tag.Tagged[Witness.`"time"`.T], Long]]]
+
 
         implicit val vv = new ReprFact[Double with labelled.KeyTag[Symbol with shapeless.tag.Tagged[Witness.`"value"`.T], Double]] {
           override def getRepr(documentSnapshot: DocumentData)(implicit ev: Aux[_]) = ???
         }
-        implicitly[ReprFact[Double with labelled.KeyTag[Symbol with shapeless.tag.Tagged[Witness.`"value"`.T], Double]]]
+
+                val r = implicitly[ReprFact[ds]]
+                // implicitly[ReprFact[Double with labelled.KeyTag[Symbol with shapeless.tag.Tagged[Witness.`"value"`.T], Double]]]
+                // val tt :ds = tbd.get[ds]
+                //val tt = fieldN
+                //  .map(fn =>(Symbol(fn), aa.get(fn)))
+                // .foldRight[HList](HNil())((a1,a2) => a1._1 ->> a1._2 :: a2)
 
 
-        val r = implicitly[ReprFact[ds]]
-        // implicitly[ReprFact[Double with labelled.KeyTag[Symbol with shapeless.tag.Tagged[Witness.`"value"`.T], Double]]]
-        // val tt :ds = tbd.get[ds]
-        //val tt = fieldN
-        //  .map(fn =>(Symbol(fn), aa.get(fn)))
-        // .foldRight[HList](HNil())((a1,a2) => a1._1 ->> a1._2 :: a2)
-
+                // new TimeValue(aa.get())
+          */
+        val gen = LabelledGeneric[TimeValue]
         val tt :ds =  ??? //r.getRepr(aa)
         gen.from(tt)
-        // new TimeValue(aa.get())
       }
       ,
       _ => ZIO.fail(new Exception())
